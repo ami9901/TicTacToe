70e8c65b50686a1fa2447b3362638b620f9c16e1
Ruchitesh Malukani <41920841+RuchiteshPM@users.noreply.github.com>
2018-10-06 20:48:04 +0530

    colour and font setup

--- app/src/main/java/com/example/ruchitesh/onlinetictactoe/MainActivity.java
+++ app/src/main/java/com/example/ruchitesh/onlinetictactoe/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.ruchitesh.onlinetictactoe;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,38 @@ public class MainActivity extends AppCompatActivity {
 
     }
     void makeChange(View view){
+        Button selected = (Button) view;
+        switch (selected.getId()){
+            case R.id.b1:
+//                Toast.makeText(this,"b1 selected",Toast.LENGTH_LONG).show();
+                selected.setBackgroundColor(Color.YELLOW);
+                selected.setText("X");
 
+                break;
+            case R.id.b2:
+                Toast.makeText(this,"b2 selected",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.b3:
+                Toast.makeText(this,"b3 selected",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.b4:
+                Toast.makeText(this,"b4 selected",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.b5:
+                Toast.makeText(this,"b5 selected",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.b6:
+                Toast.makeText(this,"b6 selected",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.b7:
+                Toast.makeText(this,"b7 selected",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.b8:
+                Toast.makeText(this,"b8 selected",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.b9:
+                Toast.makeText(this,"b9 selected",Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
